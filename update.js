058d7b17b5267ef3f689071ba1867fc16469b454
Mauricio Perdomo <32530656+maoper11@users.noreply.github.com>
2026-08-02 02:40:02 -0500

    Add package updater

---
+++ update.js
@@ -0,0 +1,19 @@
+module.exports = async () => ({
+  requires: { bundle: "ai" },
+  run: [
+    { method: "shell.run", params: { message: "git pull --ff-only" } },
+    {
+      method: "shell.run",
+      params: {
+        path: "app",
+        venv: "env",
+        message: [
+          "python -m pip install -U pip setuptools wheel",
+          "python -m pip install -U jupyterlab notebook ipykernel ipywidgets tensorboard",
+        ],
+      },
+    },
+    { method: "script.start", params: { uri: "packages.js", params: { path: "app", venv: "env" } } },
+    { method: "script.start", params: { uri: "verify.js", params: { path: "app", venv: "env" } } },
+  ],
+});
