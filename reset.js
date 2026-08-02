8be0424ba604108a0c9870c47bf981f7a78a96f2
Mauricio Perdomo <32530656+maoper11@users.noreply.github.com>
2026-08-02 03:22:13 -0500

    Simplify reset for app-contained workspace

--- reset.js
+++ reset.js
@@ -1,6 +1,7 @@
 module.exports = {
   run: [
     { method: "fs.rm", params: { path: "app" } },
+    // Remove the legacy root-level workspace from versions prior to 0.1.2.
     { method: "fs.rm", params: { path: "workspace" } },
   ],
 };
