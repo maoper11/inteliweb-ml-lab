cd117152afd98c79d1a7987e8623e153afb842f9
Mauricio Perdomo <32530656+maoper11@users.noreply.github.com>
2026-08-02 12:44:08 -0500

    Remove obsolete lesson bundle generator from install

--- install.js
+++ install.js
@@ -82,14 +82,6 @@ module.exports = async (kernel, info) => {
       params: {
         path: "app",
         venv: "env",
-        message: "python ../tools/install_lesson_bundle.py curriculum/python-to-diffusion/notebooks",
-      },
-    },
-    {
-      method: "shell.run",
-      params: {
-        path: "app",
-        venv: "env",
         message: "python ../tools/install_courses.py curriculum workspace/courses",
       },
     },
